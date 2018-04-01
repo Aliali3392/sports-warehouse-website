f0b6832a1b9a3f92d5b0e99eff57858486638a3b
char1eschen <martian2005@gmail.com>
2018-04-01 14:51:51 +1000

    update html

--- templates/layout.html.php
+++ templates/layout.html.php
@@ -9,6 +9,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <title><?= $title ?></title>
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400" rel="stylesheet">
     <link href="css/fontawesome-all.min.css" rel="stylesheet">
