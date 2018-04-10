3fd994087e34fbbe3669bf5c65b5986f5d9549a1
char1eschen <martian2005@gmail.com>
2018-04-10 22:42:45 +1000

    edit css

--- contactus.php
+++ contactus.php
@@ -61,23 +61,19 @@
 			$mail->Host = 'smtp.sendgrid.net';
 			$mail->SMTPAuth = true;
 			$mail->Username = 'apikey';
-			$mail->Password = 'Your password';
+			$mail->Password = 'input your password';
 			$mail->Port = 25;
 			$mail->From = $_POST["email"];
 			$mail->FromName = $_POST["firstName"]." ".$_POST["lastName"];
 			$mail->addReplyTo($_POST["email"], $_POST["firstName"]);
-			$mail->addAddress("Your eamil address", "Sports Warehouse");
+			$mail->addAddress("input your email", "Sports Warehouse");
 			$mail->Subject = "Sports Warehouse Questions";
 			$mail->Body = "Email: ".$_POST["email"]."\n"."Tel: ".$_POST["phone"]."\n"."Question: ".$_POST["question"]."\n"."From: ".$_POST["firstName"]." ".$_POST["lastName"];
 
 			if (!$mail->send()) {  
-				$output = ob_get_clean();
-     			ob_start();
 				include "templates/error.html.php";   
 			}    
 			else {
-				$output = ob_get_clean();
-     			ob_start();
 				include "templates/confirmation.html.php";
 			}
 		}
